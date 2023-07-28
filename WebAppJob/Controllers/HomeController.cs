369b26d8ef4735e5bb5171769b3c957adfa9d128
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-07-28 08:58:26 -0600

    Add project files.

---
+++ WebAppJob/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAppJob.Models;
+
+namespace WebAppJob.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(UserModel user)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebAppJob/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppJob.Models;
+
+namespace WebAppJob.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoginSin(UserModel user)
+        {
+            return RedirectToAction("Index","Home",user);
+        }
+    }
+}
