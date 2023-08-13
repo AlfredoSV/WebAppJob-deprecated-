430f81d5c4ee64459552e01b7c9e920a31e0ca65
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-08-13 08:48:05 -0600

    Add new tables on script Tables.sql

--- Persistence/Script DB/Tables.sql
+++ Persistence/Script DB/Tables.sql
@@ -35,4 +35,70 @@ updatedate date,
 createdate date,
 isactive bit);
 
+create table applycompetitorjob(
+id UNIQUEIDENTIFIER,
+idcompetitor uniqueidentifier,
+idjob uniqueidentifier,
+idstatus uniqueidentifier,
+dateapply date,
+idusercreated uniqueidentifier,
+updatedate date, 
+createdate date,
+isactive bit);
 
+create table statusgeneral(
+id UNIQUEIDENTIFIER,
+namestatus uniqueidentifier,
+idusercreated uniqueidentifier,
+updatedate date, 
+createdate date,
+isactive bit);
+
+create table civilstatus(
+id UNIQUEIDENTIFIER,
+namestatus uniqueidentifier,
+idusercreated uniqueidentifier,
+updatedate date, 
+createdate date,
+isactive bit);
+
+create table englishlevel(
+id UNIQUEIDENTIFIER,
+namelevel uniqueidentifier,
+idusercreated uniqueidentifier,
+updatedate date, 
+createdate date,
+isactive bit);
+
+create table competitor(
+id UNIQUEIDENTIFIER,
+namecompetitor varchar(30), 
+lastnamecompetitor varchar(30),
+birthdaydate date,
+age int,
+idenglishlevel uniqueidentifier,
+cv varchar(max),
+yearsofexperience int,
+idlastgradestudies uniqueidentifier,
+idcivilstatus uniqueidentifier,
+idrequestjob uniqueidentifier,
+idworkexpechistory uniqueidentifier,
+iduser uniqueidentifier,
+idusercreated uniqueidentifier,
+updatedate date, 
+createdate date,
+isactive bit);
+
+create table experienciework(
+id UNIQUEIDENTIFIER,
+years integer,
+idcompetitor uniqueidentifier,
+namecompany varchar(40), 
+position varchar(40),
+reasonforresignation varchar(max),
+initdate date,
+finalizationdate date,
+namedirectboss varchar(40),
+idusercreated uniqueidentifier,
+updatedate date, 
+createdate date);
